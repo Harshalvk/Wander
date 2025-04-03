2845e116d6336231ceb016f989c850228e0d0c3c
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-03 23:47:39 +0530

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text
+        style={{
+          fontFamily: "geist-bold",
+          fontSize: 30,
+        }}
+      >
+        hello
+      </Text>
+    </View>
+  );
+}
