ca13da5938bdb19a9428f965f16be1334acd5ff0
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-27 22:07:20 +0530

    feat: implement form handling for signup with validation using react-hook-form and Zod

--- app/auth/sign-up/index.tsx
+++ app/auth/sign-up/index.tsx
@@ -5,9 +5,50 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { useRouter } from "expo-router";
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
+import { SignupSchema } from "~/schemas/auth.schema";
 
 const SignUpPage = () => {
   const router = useRouter();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof SignupSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+      username: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof SignupSchema>) => {
+    try {
+      const res = await fetch(
+        `${process.env.EXPO_PUBLIC_BASE_URL}/api/signup`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        }
+      );
+
+      if (!res.ok) {
+        alert("User not created");
+        return;
+      }
+
+      router.push("/auth/sign-in");
+    } catch (error) {
+      console.error("Signup error:", error);
+      alert("Something went wrong. Please try again later.");
+    }
+  };
+
   return (
     <SafeAreaView>
       <View
@@ -45,10 +86,59 @@ const SignUpPage = () => {
             </Text>
           </View>
           <View className="w-full flex gap-6 justify-between mt-10">
-            <Input placeholder="Enter username" />
-            <Input placeholder="Enter email" />
-            <Input placeholder="Enter password" secureTextEntry={true} />
-            <Button className="bg-blue-600 rounded-xl">
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter username"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="username"
+            />
+            {errors.username && <Text>This is required.</Text>}
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter email"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="email"
+            />
+            {errors.email && <Text>This is required.</Text>}
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter password"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  textContentType="password"
+                />
+              )}
+              name="password"
+            />
+            {errors.password && <Text>This is required</Text>}
+            <Button
+              className="bg-blue-600 rounded-xl"
+              onPress={handleSubmit(onSubmit)}
+            >
               <Text className="text-[#ffff] font-semibold">Continue</Text>
             </Button>
           </View>
