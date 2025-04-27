63d6e7c4b975c8ab91258cba62bf7921139a5a46
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-27 22:06:16 +0530

    feat: implement signin API endpoint with user validation

---
+++ app/api/signin+api.ts
@@ -0,0 +1,34 @@
+import { db } from "~/lib/prisma";
+import { SigninSchema } from "~/schemas/auth.schema";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const validBody = SigninSchema.safeParse(body);
+
+    if (validBody.error) {
+      return Response.json({ err: "Invalid params" });
+    }
+
+    const user = await db.user.findUnique({
+      where: {
+        email: validBody.data.email,
+      },
+    });
+
+    const passwordMatch = bcrypt.compareSync(
+      validBody.data.password,
+      user?.password!
+    );
+
+    if (!passwordMatch) {
+      return Response.json({ err: "Invalid email or password" });
+    }
+
+    return Response.json({ msg: "Signed in successfully" });
+  } catch (error) {
+    console.error("Signin Error:", error);
+    return Response.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
