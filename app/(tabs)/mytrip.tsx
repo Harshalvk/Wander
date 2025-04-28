0286d25d4fe0691cd174f7beb22cc47cab994b49
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-28 17:34:47 +0530

    feat: enhance MyTrip component with user trips state and layout improvements

--- app/(tabs)/mytrip.tsx
+++ app/(tabs)/mytrip.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Button from "~/components/Button";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import StartNewTripCard from "~/components/MyTrips/StartNewTripCard";
 
 const MyTrip = () => {
+  const [userTrips, setUserTrips] = useState([]);
+
   return (
-    <SafeAreaView>
-      <View className="p-8">
-        <Text>MyTrip</Text>
-        <Button>
-          <Text className="text-white">Click me</Text>
-        </Button>
+    <SafeAreaView className="h-full p-6">
+      <View className="flex flex-row items-center justify-between">
+        <Text className="text-3xl font-bold">My Trip</Text>
+        <Ionicons name="add-circle" size={30} />
       </View>
+
+      {userTrips.length === 0 ? <StartNewTripCard /> : null}
     </SafeAreaView>
   );
 };
